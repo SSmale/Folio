818f5e0734e95b45f5f3bae2ac4a59d64587dc4e
SimonSmale <simon@smalemail.net>
2017-09-29 19:38:50 +0100

    mobile view working - needs content

--- app/js/home.js
+++ app/js/home.js
@@ -1,18 +1,49 @@
 $(function(){
-  if (window.matchMedia('(max-width: 1200px)').matches)
-  {
-      // do functionality on screens smaller than 768px
+  // do functionality on screens smaller than 768px
   
-      $(".icon-container").click(function(){
-        var $this = $(this);
-        if ($this.children().hasClass('active')) {
-          $this.find(".circle").toggleClass('active');
-        } else {
-          $(".circle").removeClass('active');
-          $this.find(".circle").toggleClass('active');
-        }
-      });
+  $(".icon-container").click(function(){
+    
+    var $this = $(this);
+    var $tbl = $this.data('area');
+    
+    if (window.matchMedia('(max-width: 1200px)').matches)
+    {
+
+      if ($this.children().hasClass('active')) {
+        //$this.find(".circle").toggleClass('active');
+        
+        loadElement($tbl);
+        
+      } else {
+        $(".circle").removeClass('active');
+        $this.find(".circle").toggleClass('active');
+      }
     } else {
-      alert("fired");
+      loadElement($tbl);
     }
+    
+  });
 });   
+
+
+
+$("svg").click(function(){
+  
+  var $this = $(this);
+
+  $('.site').removeClass('element');
+  
+  $('.open').removeClass('open');
+    
+});
+
+
+function loadElement($tbl){
+  
+  console.log($tbl);
+
+  $('.site').addClass('element');
+
+  $('.'+$tbl).addClass('open');
+
+}
